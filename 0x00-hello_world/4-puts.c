a2631fdaf9ef2f5e05f2b650a3bc6a258083ac9a
Daerego Braide <phronesis4xt@gmail.com>
2022-06-30 22:09:53 -0700

    task 4 adjust2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/*prints a string output using puts function */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
