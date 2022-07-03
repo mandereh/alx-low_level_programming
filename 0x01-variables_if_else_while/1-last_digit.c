474691397fbfd56048e5973a2cffbb61e35bee22
Daerego Braide <phronesis4xt@gmail.com>
2022-07-03 14:40:06 -0700

    task 1 adjust4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,8 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is %d ", n,n = n%10);
+printf("Last digit of %d is %d ", n, n%10);
+n = n % 10;
 if (n > 5)
 {
 printf("and is greater than 5");
