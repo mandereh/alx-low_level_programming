3cd941f6f041a8215ab8daf7dcc9a690009490b3
Daerego Braide <phronesis4xt@gmail.com>
2022-07-03 15:46:35 -0700

    task 2 adjust5

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 int ch;
 for (ch = 97; ch <= 122; ch++)
 {
-putchar(ch)
+putchar(ch);
 }
 putchar("\n);
 return (0);
