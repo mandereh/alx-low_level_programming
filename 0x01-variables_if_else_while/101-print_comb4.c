31233e057463dc33b37e84ab88bd8b66c426b2b4
Daerego Braide <phronesis4xt@gmail.com>
2022-07-04 07:50:14 -0700

    task 101 advanced

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,15 +7,19 @@ int main(void)
 {
 int ch;
 int n;
+int m;
 for (ch = 48; ch <= 57; ch++)
 {
-for (n = 49; n <=: 57; n++)
+for (n = 49; n <= 57; n++)
 {
-if (n > ch)
+for (m = 50; m <= 57; m++)
+{
+if (n > ch && m > n)
 {
 putchar(ch);
 putchar(n);
-if (ch != 56 || n != 57)
+putchar(m);
+if (ch != 55 || n != 56 || m != 57)
 {
 putchar(44);
 putchar(32);
@@ -23,6 +27,8 @@ putchar(32);
 }
 }
 }
+}
 putchar(10);
 return (0);
 }
+
