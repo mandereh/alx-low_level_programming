66e946f41425693f34e8ce4f0a4637fd2e2f1852
Daerego Braide <phronesis4xt@gmail.com>
2022-07-05 20:46:20 -0700

    task 100 adv

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ *  * print_times_table - prints the n times table, starting with 0
+ *   * @n: number of the times table
+*/
+void print_times_table(int n)
+{
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else if (k >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
