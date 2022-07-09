93b92a1d1230bb3b384547b7695bd79e593d1790
Daerego Braide <phronesis4xt@gmail.com>
2022-07-08 20:20:34 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * * main : execution starts here
+ * * mul -> multiplying 2 numbers
+ * * @a: interger param1
+ * * @b: integer param 2
  * * Return: success and intiger
 */
 int mul(int a, int b)
