eb4cdc85a3bcb22a4b92c793fb0537a892f22b18
Daerego Braide <phronesis4xt@gmail.com>
2022-09-18 12:40:57 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * * listint_len - a function that returns the number
- * *  of elements in a linked listint_t list
+ * * list_len - a function that returns the number
+ * *  of elements in a linked list_t list
  * * @h: input
  * * Return: 0
 */
