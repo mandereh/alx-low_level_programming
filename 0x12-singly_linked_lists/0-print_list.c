be31bb48334c27722403c5f41bbf4adf6f00958a
Daerego Braide <phronesis4xt@gmail.com>
2022-09-18 12:24:21 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 /**
- * print_listint - a function that prints all the elements of a listint_t list.
- * @h: struct
- * Return: number of nodes
+* print_list - prints all the elements of a linked list
+* @h: pointer to the list_t list to print
+* Return: the number of nodes printed
 */
-size_t print_listint(const listint_t *h)
+size_t print_list(const list_t *h)
 {
-int count = 0;
-if (h == NULL)
-return (0);
-while (h != NULL)
+size_t n = 0;
+while (h)
 {
-printf("%d\n", h->n);
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-count++;
+n++;
 }
-return (count);
+return (n);
 }
