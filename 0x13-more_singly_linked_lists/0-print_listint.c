a0a3d2c57b4caa23abc25ec6a309b094c9b78f90
Daerego Braide <phronesis4xt@gmail.com>
2022-09-18 15:40:32 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- *  * print_listint - a function that prints all the elements of a listint_t list.
- *   * @h: struct
- *    * Return: number of nodes
+ * print_listint - a function that prints all the elements of a listint_t list.
+ * @h: struct
+ * Return: number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
