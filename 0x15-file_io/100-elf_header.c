58a634af7dbab657fb286860168a3941858594f5
Daerego Braide <phronesis4xt@gmail.com>
2023-01-20 23:31:45 +0000

    adjust

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
-#include "holberton.h"
-#include <unistd.h>
-#include <stdlib.h>
+#include <elf.h>
+
 /**
- * main - Copies the content of a file to another file.
- * @ac: Argument count
- * @av: argument values
- * Return: 0 on succes, -1 on error.
+ * get_type - determine file type
+ * @ehdr: pointer to struct of ELF header features
  */
-int main(int ac, char *av[])
+void get_type(Elf64_Ehdr *ehdr)
 {
-return (1);
-return (0);
+	printf("%-35s", "Type:");
+	switch (ehdr->e_type)
+	{
+	case ET_NONE:
+		printf("NONE (Unknown type)\n");
+		break;
+	case ET_REL:
+		printf("REL (Relocatable file)\n");
+		break;
+	case ET_EXEC:
+		printf("EXEC (Executable file)\n");
+		break;
+	case ET_DYN:
+		printf("DYN (Shared object file)\n");
+		break;
+	case ET_CORE:
+		printf("CORE (Core file)\n");
+		break;
+	}
 }
+
+
+
+/**
+ * main - entry point for program to get header of ELF file
+ * @argc: arg of count
+ * @argv: arg of array
+ *
+ * Return: 1 on success, error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
+	return (1);
+}
+
